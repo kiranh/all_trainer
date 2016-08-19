51d365b8410d2097d73195f2a8fc082350bd447a
Hemant Kumar <gethemant@gmail.com>
2016-08-18 22:11:55 -0400

    Fix more bugs introduced after refactoring the code structure

--- qml_ui/common.js
+++ qml_ui/common.js
@@ -1,23 +1,10 @@
 function getSpriteImage(parent, spriteName) {
-    var component = Qt.createComponent("qrc:/sprite.qml");
+    var component = Qt.createComponent("qrc:/qml_ui/sprite.qml");
     if(component.status === Component.Ready) {
-        return component.createObject(parent,  {"source": "qrc:/" + spriteName})
+        return component.createObject(parent,  {"source": "qrc:/images/" + spriteName})
     } else {
         return null;
     }
 }
 var globalPlayer = null;
-
-function playSound(soundSource) {
-    var component = Qt.createComponent("qrc:/player.qml");
-    if(component.status === Component.Ready) {
-        if(globalPlayer !== null) {
-            globalPlayer.stop();
-        }
-        globalPlayer = component.createObject(parent,  {"source": soundSource});
-        globalPlayer.play();
-    } else {
-        return null;
-    }
-}
 
